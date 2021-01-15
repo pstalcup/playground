e079b1648f1237fc8f46641768ac22598fce1e3d
Brett <brett18@gmail.com>
2021-01-15 12:01:44 -0500

    added selection box

--- src/scene_battle_map.c
+++ src/scene_battle_map.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "raylib.h"
 #include "inc/scene.h"
 #include "inc/grid.h"
@@ -7,6 +9,21 @@ Rectangle scaledScreen;
 
 int cellSize = 50; 
 int borderSize = 2; 
+int lastClickedX = 0;
+int lastClickedY = 0;
+int lastClickedGridX = 0;
+int lastClickedGridY = 0;
+int mouseX = 0;
+int mouseY = 0;
+int boxStartX = 0;
+int boxStartY = 0;
+int boxWidth = 0;
+int boxHeight = 0;
+int boxStartGridX = -1;
+int boxStartGridY = -1;
+int boxGridWidth = 0;
+int boxGridHeight = 0;
+int boxSizeThreshold = 1;
 
 int load(void) {
     scaledScreen = (Rectangle) {0, 0, gridWidth, gridHeight}; 
@@ -16,9 +33,17 @@ int load(void) {
 }
 
 void drawGrid(void) {
+    SetMouseScale(0.02, 0.02);
     for(int x = 0; x < gridWidth; ++x) {
         for(int y = 0; y < gridHeight; ++y) {
             Color outline = (x == mouseGridX && y == mouseGridY) ? RED : BLACK;
+            if(IsMouseButtonDown(MOUSE_LEFT_BUTTON)){
+                if ((x >= boxStartGridX) && (x <= boxStartGridX+boxGridWidth) && (y >= boxStartGridY) && (y <= boxStartGridY+boxGridHeight)){
+                    outline = RED;
+                }
+                
+            }
+
             Color fill = WHITE; 
             switch(gridCell(x, y)) {
                 case 1: fill = GREEN; break;
@@ -31,17 +56,66 @@ void drawGrid(void) {
     }
 }
 
+void drawSelectionBox(void) {
+    if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
+        if ((boxWidth < boxSizeThreshold) || (boxHeight < boxSizeThreshold)) {
+            return;
+        } 
+        Rectangle boxRec = (Rectangle) {boxStartX, boxStartY, boxWidth, boxHeight}; 
+        DrawRectangleLinesEx(boxRec, 2, YELLOW);
+    }
+}
+
 int update(double delta) {
+    SetMouseScale(1.0, 1.0);
+    mouseX = (int) GetMouseX();
+    mouseY = (int) GetMouseY();
+    SetMouseScale(0.02, 0.02);
     mouseGridX = (int) GetMouseX(); 
-    mouseGridY = (int) GetMouseY(); 
+    mouseGridY = (int) GetMouseY();
 
     bool insideScreen = CheckCollisionPointRec(GetMousePosition(), scaledScreen);
     int retCode = 0; 
     
     if (insideScreen) {
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            lastClickedX = mouseX;
+            lastClickedY = mouseY;
+            lastClickedGridX = mouseGridX;
+            lastClickedGridY = mouseGridY;
+        } 
         if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
-            setGridCell(mouseGridX, mouseGridY, 0); 
+
+            /* set the box starting location, using the min for x and y guarantees we end up with the top left corner */
+            boxStartX = mouseX;
+            boxStartY = mouseY;
+            if (lastClickedX < boxStartX) boxStartX = lastClickedX;
+            if (lastClickedY < boxStartY) boxStartY = lastClickedY;
+            
+            boxWidth = abs(mouseX - lastClickedX);
+            boxHeight = abs(mouseY - lastClickedY);
+
+            /* repeat this for the grid-scaled mouse x,y values so we can determine which cells fall into the selection */
+            boxStartGridX = mouseGridX;
+            boxStartGridY = mouseGridY;
+            if (lastClickedGridX < boxStartGridX) boxStartGridX = lastClickedGridX;
+            if (lastClickedGridY < boxStartGridY) boxStartGridY = lastClickedGridY;
+            
+            boxGridWidth = abs(mouseGridX - lastClickedGridX);
+            boxGridHeight = abs(mouseGridY - lastClickedGridY);
         }
+        if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
+            if ((boxStartGridX) >= 0 && (boxStartGridY >= 0)){
+                for(int x = 0; x <= boxGridWidth; x++){ //note boxGridWidth/Height can be 0, which really means 1 cell
+                    for(int y = 0; y <= boxGridHeight; y++){
+                        setGridCell(boxStartGridX + x, boxStartGridY + y, 0);
+                    }
+                }
+            }
+        }
+    }
+    if (IsKeyDown(KEY_Y)) {
+        setGridCell(mouseGridX, mouseGridY, 0);
     }
     if (IsKeyDown(KEY_A)) {
         clearGrid();
@@ -60,6 +134,7 @@ int update(double delta) {
 
 void render() {
     drawGrid(); 
+    drawSelectionBox();
 }
 
 int unload() {
